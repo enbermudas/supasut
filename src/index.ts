bf415638d800987b44515a9b31245b0d606eb9bd
Enrique Bermúdez <enbermudez96@gmail.com>
2023-07-31 19:16:43 -0300

    feat: implement uniq function

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import omit from "./omit";
 import pluck from "./pluck";
+import uniq from "./uniq";
 
 export default {
   omit,
   pluck,
+  uniq,
 };
---
+++ src/uniq.ts
@@ -0,0 +1,12 @@
+/**
+ * Retrieves unique elements from an array by eliminating duplicates.
+ *
+ * @template T - The type of elements in the array.
+ * @param {T[]} array - The array from which to get unique elements.
+ * @returns {T[]} - An array containing unique elements from the input array.
+ */
+function uniq<T>(array: T[]): T[] {
+  return Array.from(new Set(array));
+}
+
+export default uniq;
