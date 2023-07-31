b11e50b509ac703020f271b1c469c773bed87c0a
Enrique Bermúdez <enbermudez96@gmail.com>
2023-07-31 20:04:38 -0300

    fix: occurrences naming

--- __tests__/occurrences.test.ts
+++ __tests__/occurrences.test.ts
@@ -1,38 +1,38 @@
 import occurrences from "../src/occurrences";
 
-describe("occurrences function", () => {
+describe("occurrences", () => {
   it("should count the number of occurrences of the substring in the string", () => {
     const str = "hello, hello, hello world";
     const substring = "hello";
-    const occurrences = occurrences(str, substring);
+    const occ = occurrences(str, substring);
     expect(occurrences).toBe(3);
   });
 
   it("should return 0 if the substring is not found", () => {
     const str = "hello, hello, hello world";
     const substring = "none";
-    const occurrences = occurrences(str, substring);
+    const occ = occurrences(str, substring);
     expect(occurrences).toBe(0);
   });
 
   it("should handle an empty string as input", () => {
     const str = "";
     const substring = "hello";
-    const occurrences = occurrences(str, substring);
+    const occ = occurrences(str, substring);
     expect(occurrences).toBe(0);
   });
 
   it("should handle an empty substring as input", () => {
     const str = "hello, hello, hello world";
     const substring = "";
-    const occurrences = occurrences(str, substring);
+    const occ = occurrences(str, substring);
     expect(occurrences).toBe(26);
   });
 
   it("should handle case-insensitive occurrences", () => {
     const str = "Hello, hello, Hello world";
     const substring = "hello";
-    const occurrences = occurrences(str, substring);
+    const occ = occurrences(str, substring);
     expect(occurrences).toBe(3);
   });
 });
